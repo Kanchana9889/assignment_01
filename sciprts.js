eaf574178ae7c91e5592100ade0fa02bf6179dd7
Kanchana9889 <148602337+Kanchana9889@users.noreply.github.com>
2023-12-02 15:01:11 +0530

    left right center button and js

---
+++ sciprts.js
@@ -0,0 +1,27 @@
+function left(){
+    document.querySelectorAll('.left').forEach(function(element){
+element.addEventListener('click',function(){
+    document.getElementById('containereditor').style.textAlign='left'
+})
+
+    })
+}
+
+function center(){
+    document.querySelectorAll('.center').forEach(function(element){
+element.addEventListener('click',function(){
+    document.getElementById('containereditor').style.textAlign='center'
+})
+
+    })
+}
+
+function right(){
+    document.querySelectorAll('.right').forEach(function(element){
+element.addEventListener('click',function(){
+    document.getElementById('containereditor').style.textAlign='right'
+})
+
+    })
+}
+
